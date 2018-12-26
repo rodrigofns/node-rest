8023a7c1e0b44dde04489e72402da2833331b335
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2018-12-26 14:49:04 -0200

    Modular wiki data treatment.

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,26 +1,61 @@
 const axios = require('axios');
 
 const wiki = (app) => {
-	app.get('/wiki/:s', (req, res) => {
-		const search = req.params.s;
-
+	function queryWiki(req, res, parms, onDone) {
 		axios.get('https://en.wikipedia.org/w/api.php', {
 			params: {
-				action: 'query',
-				prop: 'extracts',
-				format: 'json',
-				exintro: '',
-				titles: search
+				...parms
 			}
 		}).then(resp => {
-			res.status(200).send({
-				searched: resp.data.query.normalized,
-				found: resp.data.query.pages
-			});
+			onDone(resp.data);
 		}).catch(err => {
 			res.status(400).send(err);
 		});
+	}
 
+	app.get('/wiki/head/:s', (req, res) => {
+		const parms = {
+			action: 'query',
+			prop: 'extracts',
+			format: 'json',
+			exintro: '',
+			titles: req.params.s
+		};
+
+		queryWiki(req, res, parms,
+			data => {
+				res.status(200).send({
+					searched: data.query.normalized,
+					found: data.query.pages
+				});
+			});
+	});
+
+	app.get('/wiki/full/:s', (req, res) => {
+		const parms = {
+			action: 'query',
+			prop: 'revisions',
+			rvprop: 'content',
+			format: 'json',
+			titles: req.params.s
+		};
+
+		queryWiki(req, res, parms,
+			data => {
+				const page = data.query.pages[ Object.keys(data.query.pages)[0] ];
+				const bulk = page.revisions[0];
+
+				res.status(200).send({
+					searched: data.query.normalized[0],
+					found: {
+						pageid: page.pageid,
+						title: page.title,
+						format: bulk.contentformat,
+						contentmodel: bulk.contentmodel,
+						body: bulk['*']
+					}
+				});
+			});
 	});
 };
 
