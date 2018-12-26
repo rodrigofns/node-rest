ec95460582c176b5e5fcb67ea369f158629e4bc6
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2018-12-26 14:20:36 -0200

    Axios descritive GET parameters.

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,19 +3,24 @@ const axios = require('axios');
 const wiki = (app) => {
 	app.get('/wiki/:s', (req, res) => {
 		const search = req.params.s;
-		const url = 'https://en.wikipedia.org/w/api.php'
-			+ '?action=query&prop=extracts&format=json&exintro=&titles=' + search;
 
-		axios.get(url)
-			.then(resp => {
-				res.status(200).send({
-					searched: resp.data.query.normalized,
-					found: resp.data.query.pages
-				});
-			})
-			.catch(err => {
-				res.status(400).send(err);
+		axios.get('https://en.wikipedia.org/w/api.php', {
+			params: {
+				action: 'query',
+				prop: 'extracts',
+				format: 'json',
+				exintro: '',
+				titles: search
+			}
+		}).then(resp => {
+			res.status(200).send({
+				searched: resp.data.query.normalized,
+				found: resp.data.query.pages
 			});
+		}).catch(err => {
+			res.status(400).send(err);
+		});
+
 	});
 };
 
