2b04661f897f3ff2087ecdf3fc47f37118b192ec
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2018-12-26 16:42:11 -0200

    Treating not found wiki page.

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,60 +1,67 @@
 const axios = require('axios');
 
 const wiki = (app) => {
-	function queryWiki(req, res, parms, onDone) {
-		axios.get('https://en.wikipedia.org/w/api.php', {
-			params: {
-				...parms
-			}
-		}).then(resp => {
-			onDone(resp.data);
-		}).catch(err => {
-			res.status(400).send(err);
-		});
+	function queryWiki(response, params, onDone) {
+		axios.get('https://en.wikipedia.org/w/api.php', {params})
+			.then(resp => onDone(resp.data))
+			.catch(err => response.status(400).send(err));
 	}
 
-	app.get('/wiki/head/:s', (req, res) => {
-		const parms = {
+	app.get('/wiki/head/:s', (request, response) => {
+		const params = {
 			action: 'query',
 			prop: 'extracts',
 			format: 'json',
 			exintro: '',
-			titles: req.params.s
+			titles: request.params.s
 		};
 
-		queryWiki(req, res, parms,
+		queryWiki(response, params,
 			data => {
-				res.status(200).send({
+				response.status(200).send({
 					searched: data.query.normalized,
 					found: data.query.pages
 				});
 			});
 	});
 
-	app.get('/wiki/full/:s', (req, res) => {
-		const parms = {
+	app.get('/wiki/full/:s', (request, response) => {
+		const params = {
 			action: 'query',
 			prop: 'revisions',
 			rvprop: 'content',
 			format: 'json',
-			titles: req.params.s
+			titles: request.params.s
 		};
 
-		queryWiki(req, res, parms,
+		queryWiki(response, params,
 			data => {
-				const page = data.query.pages[ Object.keys(data.query.pages)[0] ];
-				const bulk = page.revisions[0];
+				const pageid = Object.keys(data.query.pages)[0];
+				const page = data.query.pages[pageid];
+				let out = {
+					searched: data.query.normalized[0]
+				};
 
-				res.status(200).send({
-					searched: data.query.normalized[0],
-					found: {
-						pageid: page.pageid,
-						title: page.title,
-						format: bulk.contentformat,
-						contentmodel: bulk.contentmodel,
-						body: bulk['*']
-					}
-				});
+				if (pageid == -1) {
+					out = {
+						...out,
+						status: 'missing (pageid: -1)'
+					};
+				} else {
+					const bulk = page.revisions[0];
+					out = {
+						...out,
+						found: {
+							pageid: page.pageid,
+							title: page.title,
+							format: bulk.contentformat,
+							contentmodel: bulk.contentmodel,
+							body: bulk['*']
+						}
+					};
+				}
+
+				response.status(200).send(out);
 			});
 	});
 };
